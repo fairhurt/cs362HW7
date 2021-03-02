9dd3291d7285a440049ea1367335518c1cd18da5
fairhurt <55992872+fairhurt@users.noreply.github.com>
2021-03-02 10:30:16 -0800

    Added code to make the tests for leap year work properly.

--- leap_year_hw7.py
+++ leap_year_hw7.py
@@ -1,3 +1,7 @@
 #
 def check_leap_year(arg):
-    pass
+    year = arg;
+    if((year % 4==0 and year % 100 !=0) or (year % 400 ==0)):
+        return True
+    else:
+        return False
--- test_leap_year.py
+++ test_leap_year.py
@@ -5,9 +5,11 @@ from leap_year_hw7 import check_leap_year
 
 class TestLeapYear(unittest.TestCase):
     def test1 (self):
-        self.assertEqual(check_leap_year(2000), False)
+        self.assertEqual(check_leap_year(2000), True)
     def test2 (self):
         self.assertEqual(check_leap_year(2020), True)
+    def test3 (self):
+        self.assertEqual(check_leap_year(2023), False)
    
     
         
