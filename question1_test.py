dcb3fbf07044c845cc684abb4ba344a90cff7952
fairhurt <55992872+fairhurt@users.noreply.github.com>
2021-03-02 10:19:26 -0800

    Added the code to make the test work. The code returns true once all the values have been printed from 1 -100

--- question1_test.py
+++ question1_test.py
@@ -2,21 +2,15 @@ import unittest
 
 from question1_code import print_multiples
 
-class TestAverage(unittest.TestCase):
-    def test1 (self):
-        count = 0
-        while count < 100:
-            if(count %3 == 0 and count % 5 ==0):
-                self.assertEqual(print_multiples(count),'FizzBuzz')
-            elif(count % 3 == 0):
-                self.assertEqual(print_multiples(count),'Fizz')
-            elif(count % 5 ==0):
-                self.assertEqual(print_multiples(count),'Buzz')
-            else:
-                self.assertEqual(print_multiples(count), count)
-            count = count + 1
-    
+class TestMultiples(unittest.TestCase):
+      
+    def test1(self):
+        num=list(range(1,101))
+        self.assertEqual(print_multiples(num), True)
+        
+      
+
         
   
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+   unittest.main(verbosity=2)
